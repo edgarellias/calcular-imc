d1dc7ae8c484373b8c0e978aecc5d29acb7d34f7
Edgar Elias <edgar_jk3@hotmail.com>
2022-09-05 15:23:17 -0300

    termino de ajustes da validação

--- scripts.js
+++ scripts.js
@@ -5,25 +5,25 @@ let btn = document.querySelector("#container__button");
 let result = document.querySelector("#container__result");
 
 if (age != "" && height != "" && weight != "") {
-	btn.addEventListener("click", function () {
+	btn.addEventListener("click", function (e) {
 		const imcValue = weight.value / (height.value * height.value);
-		if ((imcValue < 18, 5)) {
+		if(imcValue < 18.5){
 			result.textContent = `Seu imc é : ${imcValue.toFixed(1)} e você está muito magro`;
-		}
-		if ((imcValue > 18, 5 && imcValue < 24, 9)) {
+		} if(imcValue > 18.5) {
 			result.textContent = `Seu imc é : ${imcValue.toFixed(1)} e você está no peso normal`;
-		}
-		if ((imcValue >= 25 && imcValue <= 29, 9)) {
+		} if (imcValue >= 25 && imcValue <= 29.9){
 			result.textContent = `Seu imc é : ${imcValue.toFixed(1)} e você está com sobrepeso`;
-		}
-		if ((imcValue >= 30 && imcValue <= 34, 9)) {
+		} if (imcValue >= 30 && imcValue <= 34.9){
 			result.textContent = `Seu imc é : ${imcValue.toFixed(1)} e você está com obesidade grau I`;
-		}
-		if ((imcValue >= 35 && imcValue <= 39, 9)) {
+		} if(imcValue >= 35 && imcValue <= 39.9) {
 			result.textContent = `Seu imc é : ${imcValue.toFixed(1)} e você está com obesidade grau II`;
-		}
-		if (imcValue > 40) {
+		} if(imcValue > 40){
 			result.textContent = `Seu imc é : ${imcValue.toFixed(1)} e você está com obesidade grau III`;
-		}
+		} 
+		e.preventDefault();
+		weight.value = "";
+		height.value = "";
+		age.value = "";
 	});
-}
+} 
+
